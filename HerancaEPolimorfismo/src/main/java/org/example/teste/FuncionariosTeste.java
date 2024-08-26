af95cb12f5af3c38e12d7105b7063951ff3fc051
Breno Canguçu <brenocangucu@gmail.com>
2024-08-25 22:29:24 -0300

    Teste RegistroDeFuncionarios e Funcionarios

--- HerancaEPolimorfismo/src/main/java/org/example/teste/FuncionariosTeste.java
+++ HerancaEPolimorfismo/src/main/java/org/example/teste/FuncionariosTeste.java
@@ -1,8 +1,14 @@
 package org.example.teste;
 
+import org.example.comissoes.Cargo;
+import org.example.comissoes.Gerente;
+import org.example.comissoes.Supervisor;
+import org.example.comissoes.Vendedor;
+
 import org.example.escolaridade.EnsinoBasico;
 import org.example.escolaridade.EnsinoMedio;
 import org.example.escolaridade.Graduacao;
+
 import org.example.funcionarios.Funcionario;
 import org.example.funcionarios.RegistroDeFuncionarios;
 
@@ -14,30 +20,72 @@ public class FuncionariosTeste {
         EnsinoBasico ensinoBasica = new EnsinoBasico("Ensino Básico");
         EnsinoMedio ensinoMedio = new EnsinoMedio("Ensino Médio");
         Graduacao graduacao = new Graduacao("Universidade");
+        Cargo gerente = new Gerente();
+        Cargo supervisor = new Supervisor();
+        Cargo vendedor = new Vendedor();
 
-        //4 funcionarios que possuem ensino básico
-        Funcionario funcionario1 = new Funcionario("João1", ensinoBasica);
-        Funcionario funcionario2 = new Funcionario("João2", ensinoBasica);
-        Funcionario funcionario3 = new Funcionario("João3", ensinoBasica);
-        Funcionario funcionario4 = new Funcionario("João4", ensinoBasica);
+        //4 funcionarios que possuem ensino básico (40%)
+        Funcionario funcionario1 = new Funcionario("Joao1", ensinoBasica);
+        Funcionario funcionario2 = new Funcionario("Joao2", ensinoBasica);
+        Funcionario funcionario3 = new Funcionario("Joao3", ensinoBasica);
+        Funcionario funcionario4 = new Funcionario("Joao4", ensinoBasica);
 
-        //4 funcionarios que possuem ensino médio
-        Funcionario funcionario5 = new Funcionario("João5", ensinoMedio);
-        Funcionario funcionario6 = new Funcionario("João6", ensinoMedio);
-        Funcionario funcionario7 = new Funcionario("João7", ensinoMedio);
-        Funcionario funcionario8 = new Funcionario("João8", ensinoMedio);
+        //4 funcionarios que possuem ensino médio (40%
+        Funcionario funcionario5 = new Funcionario("Joao5", ensinoMedio);
+        Funcionario funcionario6 = new Funcionario("Joao6", ensinoMedio);
+        Funcionario funcionario7 = new Funcionario("Joao7", ensinoMedio);
+        Funcionario funcionario8 = new Funcionario("Joao8", ensinoMedio);
 
-        //2 funcionarios que possuem graduação
-        Funcionario funcionario9 = new Funcionario("João9", graduacao);
-        Funcionario funcionario10 = new Funcionario("João10", graduacao);
+        //2 funcionarios que possuem graduação (20%)
+        Funcionario funcionario9 = new Funcionario("Joao9", graduacao);
+        Funcionario funcionario10 = new Funcionario("Joao10", graduacao);
 
         for (Funcionario funcionario : Arrays.asList(funcionario1, funcionario2, funcionario3, funcionario4, funcionario5, funcionario6, funcionario7, funcionario8, funcionario9, funcionario10)) {
             registro.addFuncionario(funcionario);
         }
 
-        System.out.println("Custo total: " + registro.custosDeSalario());
+        //Exercício 07 - Calcule os custos da empresa com salários totais e por nível de escolaridade,
+        //utilize a classe funcionário desenvolvida no exercício anterior.
+        System.out.println("Total Custo de Salarios (sem cargos comissionados): R$" + registro.salariosTotal());
         System.out.println();
 
-        System.out.println("Custo por Escolaridade: " + registro.salariosPorEscolaridade());
+        System.out.println(registro.salariosPorEscolaridade());
+        System.out.println();
+        //Exercício 10 - Refaça o exercício 7 considerando que 10% dos funcionários são
+        //Gerentes, 20% são supervisores e 70% são vendedores.
+
+        //1 funcionário gerente (10%)
+        funcionario1.setCargo(gerente);
+
+        //2 funcionários supervisores (20%)
+        funcionario2.setCargo(supervisor);
+        funcionario3.setCargo(supervisor);
+
+        //7 funcionários vendedores (70%)
+        funcionario4.setCargo(vendedor);
+        funcionario5.setCargo(vendedor);
+        funcionario6.setCargo(vendedor);
+        funcionario7.setCargo(vendedor);
+        funcionario8.setCargo(vendedor);
+        funcionario9.setCargo(vendedor);
+        funcionario10.setCargo(vendedor);
+
+        //Calcule os custos da empresa com salários totais e por nível de escolaridade,
+        //utilize a classe funcionário desenvolvida no exercício anterior
+
+        System.out.println("Total Custo de Salarios (com cargos comissionados): R$" + registro.salariosTotal());
+        System.out.println();
+
+        System.out.println(registro.salariosPorEscolaridade());
+        System.out.println();
+
+        //Exercício 11 Sobreescreva o método toString de forma que ele imprima o nome do
+        //funcionário, a comissão e o salário total. Imprima todos os
+        //funcionários da empresa criada no exercício 7
+
+        for(Funcionario funcionario: registro.getFuncionarios()){
+            System.out.println(funcionario);
+            System.out.println();
+        }
     }
 }
